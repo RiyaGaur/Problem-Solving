2f36b4570c828ebba95a1388bdf9432d431939cf
RiyaGaur <44978076+RiyaGaur@users.noreply.github.com>
2020-06-24 12:02:01 +0530

    Create solution.c

---
+++ Simple Array Sum/solution.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    printf("%d",sum);
+    return 0;
+
+}

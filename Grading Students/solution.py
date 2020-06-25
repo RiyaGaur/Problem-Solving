4850ee6e00fa8b63b039140aae6844f25dd7b84d
RiyaGaur <44978076+RiyaGaur@users.noreply.github.com>
2020-06-25 10:06:25 +0530

    Create solution.py

---
+++ Grading Students/solution.py
@@ -0,0 +1,43 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    x = []
+    for grade in grades:
+        g = str(grade)
+        if int(g[-1])>=3 and int(g[-1])<=5 and int(g)>=38:
+            x.append(g[:-1]+'5')
+        elif int(g[-1])>7 and int(g[-1])<10 and int(g)>=38:
+            x.append(str(int(g[:-1])+1)+'0')
+        else:
+            x.append(g)
+    return x
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

da1310c471fcf02f083cbb2382fcba4c06e3511c
RiyaGaur <44978076+RiyaGaur@users.noreply.github.com>
2020-06-25 11:31:31 +0530

    Create solution.py

---
+++ Viral Advertising/solution.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the viralAdvertising function below.
+def viralAdvertising(n):
+    l = [5]
+    for i in range(n-1):
+        x = (l[i]//2)*3
+        l.append(x)
+    f = [y//2 for y in l]
+    return sum(f)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
